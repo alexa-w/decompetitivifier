27899b94d331b872c132a7d205f58e8dd416d3a4
alexa <aeginidae@gmail.com>
2023-12-14 14:09:47 -0600

    debugging lol

--- decompetitivify.js
+++ decompetitivify.js
@@ -1,6 +1,8 @@
+console.log('loaded')
 const leaderboardHeader = [...document.querySelectorAll('h1')].find(el => el.innerText === 'Leaderboards')
 
 if (leaderboardHeader) {
+    console.log('element found')
     const leaderboard = leaderboardHeader.parentElement()
     leaderboard.setAttribute('style', 'display: none;')
 }
