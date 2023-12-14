8796125cfd1b3a2bdc8ab73db20deeccf4f78291
Alexa Welch <aeginidae@gmail.com>
2023-12-14 15:16:54 -0600

    actual v1

--- decompetitivify.js
+++ decompetitivify.js
@@ -1,31 +1,33 @@
-// inefficient, but should work
-document.onchange(() => {
+const observer = new MutationObserver(function () {
     // classnames are dynamically generated, no ids are present
     // so, we need to query by inner text
-
     const headers = [...document.querySelectorAll('h1')]
 
     // leaderboard
-    const leaderboardHeader = headers.find(el => el.innerText === 'Leaderboards')
-    
-    if (leaderboardHeader) {
-        // actual container
-        const leaderboard = leaderboardHeader.parentElement()
-        // if leaderboard already hidden, do nothing, avoids infinite loop
-        if (!leaderboard.getAttribute('style').match(/display: none/).length) {
-            leaderboard.setAttribute('style', 'display: none;')
-        }
-    }
+    const leaderboardHeaders = headers.filter(el => el.innerText === 'Leaderboards')
 
     // social news
-    const socialNewsHeader = headers.find(el => el.innerText === 'Social News')
+    const socialNewsHeaders = headers.filter(el => el.innerText === 'Social News')
+    
+    if (leaderboardHeaders.length) {
+        leaderboardHeaders.forEach(leaderboardHeader => {
+            const leaderboard = leaderboardHeader.parentElement
+            // if leaderboard already hidden, do nothing
+            if (!leaderboard.getAttribute('style').match(/display: none/)) {
+                leaderboard.setAttribute('style', 'display: none;')
+            }
+        })
+    }
 
-    if (socialNewsHeader) {
-        // actual container, nested two levels deep
-        const socialNews = socialNewsHeader.parentElement.parentElement
-        // if already hidden, do nothing, avoids infinite loop
-        if (!socialNews.getAttribute('style').match(/display: none/).length) {
-            socialNews.setAttribute('style', 'display: none;')
-        }
+    if (socialNewsHeaders.length) {
+        socialNewsHeaders.forEach(socialNewsHeader => {
+            const socialNews = socialNewsHeader.parentElement.parentElement
+            // if already hidden, do nothing
+            if (!socialNews.getAttribute('style').match(/display: none/)) {
+                socialNews.setAttribute('style', 'display: none;')
+            }
+        })
     }
 })
+
+observer.observe(document, { childList: true, subtree: true })
